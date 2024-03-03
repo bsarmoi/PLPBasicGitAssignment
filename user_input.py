e773df068e3a42d7aafc9fe3072687430e000c70
bsarmoi <bsarmoi@gmail.com>
2024-03-03 21:54:28 +0300

    python code

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# program that requests users info
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
